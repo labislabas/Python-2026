25332138883af323c902cc6a377cbde31c773b25
labislabas <maykolrafael.coronado@alumnos.ulagos.cl>
2026-05-29 12:48:39 -0400

    Ejercicios

---
+++ Ejercicios /ejercicio2.1.py
@@ -0,0 +1,33 @@
+manana = float(input("Ingrese el consumo de RAM por la mañana: "))
+print(manana)
+
+medio = float(input("Ingrese el consumo de RAM en el mediodía: "))
+print(medio)
+
+tarde = float(input("Ingrese el consumo de RAM por la tarde: "))
+print(tarde)
+
+noche = float(input("Ingrese el consumo de RAM por la noche: "))
+print(noche)
+
+ram = list([manana, medio, tarde, noche])
+
+consumo_ma = ram[0]
+consumo_me = ram[1]
+consumo_ta = ram[2]
+consumo_no = ram[3]
+
+
+consumo_total = (consumo_ma + consumo_me +consumo_ta + consumo_no)
+print(f"El consumo total de RAM es: {consumo_total}")
+
+promedio = consumo_total / 4
+print(f"El promedio del consumo de la RAM es: {promedio}")
+
+max_ram = max(ram)
+print(f"El valor maximo de la RAM ingresado de manera individual es: {max_ram}")
+
+
+
+
+
