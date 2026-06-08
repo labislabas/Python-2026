917be14da020d8ecb03a740077370331fe870ad0
Maykol Coronado Risco <maykolrafael.coronado@alumnos.ulagos.cl>
2026-06-08 13:01:35 -0400

    Se agrega metodo diccionarios

---
+++ 06-diccionarios.py
@@ -0,0 +1,11 @@
+# DICCIONARIOS 
+
+paciente = {
+    'nombre' : 'Benjamin Bahamonde',
+    'edad' : 18,
+    'ciudad' : 'Ancud',
+    'Fechas_atencion' : [5,8,12],
+    'diagnostico' : 'resfrio comun'
+}
+
+print(f"=== FICHA PACIENTE === \n {paciente}")
