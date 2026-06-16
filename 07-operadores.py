c9e1345ef9c6c6a4bf28a65bcea3e2b4dfdff702
Maykol Coronado Risco <maykolrafael.coronado@alumnos.ulagos.cl>
2026-06-16 14:59:54 -0400

    Se cambia el nombre del archivo anteriormente llamado "08-operadores"

---
+++ 07-operadores.py
@@ -0,0 +1,74 @@
+# OPERADORES EN PYTHON 
+a = 10
+b =  5
+c = 4
+d= 10
+
+print ("=== OPERADORES ARITMETICOS ===")
+print (f"La suma entre la variable a y b es: {a + b}")   # operador de suma
+print (f"La resta entre la variable a y b es: {a - b}")     # operador de resta
+print (f"La multiplicacion entre la variable a y b es: {a * b}")     # operador de producto
+print (f"La division entre la variable a y b es: {a / b}")   # operador de division
+print (f"El modulo entre la variable a y b es: {a % b}")    # operador de modulo
+print (f"El coeficiente entre la variable a y b es: {a //+ b}")     # operador de division entera
+print (f"El resultado de b elevado a c (5^4): {b ** c}")    # operador de potencia -> pow() hace
+
+# se puede hacer esta operacion?
+print ("Hola" * (int((10*2) / 5 )), "\n")
+
+print("=== OPERADORES DE COMPARACION ===") # La salida es un booleano (True o False)
+print (a == b ) # op igualdad
+print (a != b ) # op desigualdad, distinto o diferente 
+print (a > b ) # op mayor que
+print (a < b ) # op menor que
+print (a >= d ) # op mayor o igual que 
+print (a <= d ) # op menor o igual que
+
+print("=== OPERADORES LOGICOS ===")
+"""" Trabajaremos con el siguiente ejemplo
+    Tenemos un vehiculo que tiene bencina (variable_bencina) y una opcion
+    que dice si esta encendido o no el vehiculo(variable encendido).
+    Dependiendo del estado de cada variable, se ira cambiando por False o True """
+
+# Variables Booelanos
+bencina = True
+encendido = True
+edad = 19
+
+# if = si
+# else = sino
+# or = o
+
+#  Utilizando el Operador AND
+if bencina and encendido:
+    print("El vehiculo puede arrancar")
+else:
+    print("E vehiculo no puede arrancar ")    
+
+
+    # Utilizando el Operador  Or
+    if bencina or encendido:
+        print("El vehiculo puede arrancar")
+    else:
+        print("El vehiculo no puede arrancar")
+
+# UTILIZANDO EL OPERADOR NOT JUNTO CON EL AND
+
+if not bencina and encendido:
+    print("El vehiculo puede arrancar")
+else:
+    print("El vehiculo no puede arrancar")
+
+# UTILIZANDO EL OPERADOR NOT JUNTO AL OR    
+
+if not bencina or encendido:
+    print("El vehiculo puede arrancar")
+else:
+    print("El vehiculo no puede arrancar")
+
+# UTILZANDO EL OPERADOR NOT JUNTO AL OPERADOR AND Y OR
+if (not bencina or encendido) or (encendido and edad >= 18):
+    print("El vehiculo puede arrancar")
+else:
+    print("El vehiculo no puede arrancar") 
+      
