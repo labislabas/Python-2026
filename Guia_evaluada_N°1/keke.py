970f9618fb590b841f0683004ea63b3b9ad1f8b9
labislabs <maykolrafael.coronado@alumnos.ulagos.cl>
2026-09-25 01:30:01 -0300

    se agregan codigos y resoluciones de codigos vistos en clases

---
+++ Guia_evaluada_N°1/keke.py
@@ -0,0 +1,10 @@
+celo = 0
+kekekito_la_cobra = int(input("Ingrese un numero para de sumar con el anterior (0 o salir para terminar): "))
+
+while kekekito_la_cobra > 0:
+    print(kekekito_la_cobra)
+    celo += kekekito_la_cobra
+
+    if kekekito_la_cobra <= 0:
+        print('Pelo que dicex')
+print('No te lo puedo creer bife')
